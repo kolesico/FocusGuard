78f8e0f72209a3b852b218c009a6d439dc9b44a9
Nick-PC <eliseevkolia654@yandex.ru>
2025-10-19 23:09:54 +0300

    add main file

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"log"
+	"time"
+
+)
+
+func main() {
+	appName := flag.String("app", "Telegram.exe", "name of app for monitor")
+
+	flag.Parse()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	events := monitor.RunMonitor(ctx, *appName)
+
+	for event := range events {
+		log.Printf("%s: %s %s", event.Timestamp.Format(time.RFC3339), *appName, event.Type)
+	}
+}
